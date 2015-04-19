630ac7a57fa2aa22dba0e8f0f7082f9a0e1f05e4
lesguillemets <lesguillemets@users.noreply.github.com>
2015-04-20 01:21:29 +0900

    simple yo-back functionalities

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python2
+from __future__ import unicode_literals
 from flask import Flask
 from flask import request
+from flask import json
+import requests
+
+from consts import yoback_token
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/yoback", methods=["GET","POST"])
+def yoback():
+    user = request.form['username']
+    requests.post(
+        'http://api.justyo.co/yo/',
+        data = {'api_token': yoback_token, 'username': user}
+    )
+    return("OK")
